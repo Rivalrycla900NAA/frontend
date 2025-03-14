443dfa0841ea57b40f45bac131b5b688edf29f27
vidhiibisla <vidhibisla18@gmail.com>
2025-03-14 00:21:15 -0400

    Initial commit

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import UserProfile from './user-profile-page.jsx';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+    <React.StrictMode>
+        <UserProfile />
+    </React.StrictMode>
+);
