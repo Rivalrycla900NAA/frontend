ad7f4722ba5c506e0cf7e01bf7caf137a3117c07
vidhiibisla <vidhibisla18@gmail.com>
2025-03-22 12:33:12 -0400

    updating app.js to add charity pulse and modify navbar

--- src/App.js
+++ src/App.js
@@ -5,17 +5,22 @@ import Welcome from "./pages/Welcome";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Dashboard from "./pages/Dashboard";
+import HomePage from "./pages/HomePage"; // ✅ Import your homepage
 
 function App() {
   return (
     <Router>
       <NavBar>
-        <NavLink to="/">Home</NavLink>
-        <NavLink to="/login">Login</NavLink>
-        <NavLink to="/signup">Sign Up</NavLink>
+        <Brand to="/">Charity Pulse</Brand> {/* ✅ Moved to left */}
+        <NavLinks>
+          <NavLink to="/login">Login</NavLink>
+          <NavLink to="/signup">Sign Up</NavLink>
+          <NavLink to="/dashboard">Dashboard</NavLink>
+        </NavLinks>
       </NavBar>
+
       <Routes>
-        <Route path="/" element={<Welcome />} />
+        <Route path="/" element={<HomePage />} /> {/* ✅ Default route */}
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/dashboard" element={<Dashboard />} />
@@ -26,17 +31,30 @@ function App() {
 
 export default App;
 
-// Styled Components for Navbar
+// ✅ Styled Components
 const NavBar = styled.nav`
   display: flex;
-  justify-content: center;
+  align-items: center;
+  justify-content: space-between;
   background: #222;
   padding: 1rem;
 `;
 
+const Brand = styled(Link)`
+  color: white;
+  font-size: 22px;
+  font-weight: bold;
+  text-decoration: none;
+  margin-left: 20px;
+`;
+
+const NavLinks = styled.div`
+  display: flex;
+  gap: 15px;
+`;
+
 const NavLink = styled(Link)`
   color: white;
-  margin: 0 15px;
   text-decoration: none;
   font-size: 18px;
 
